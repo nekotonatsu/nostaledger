7fe6f9138bd4ff9329018759b4fe1ab359d237c0
molhot <sakata19991214@gmail.com>
2026-04-04 23:13:30 +0900

    追加したモデルを操作するサービスを追加

---
+++ backend/app/Services/DailyExpenditureTagService.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\DailyExpenditureTag;
+use Illuminate\Database\Eloquent\Collection;
+use Carbon\Carbon;
+
+class DailyExpenditureTagService
+{
+    public function getAllUserDailyExpenditureTag(int $userId): Collection
+    {
+        return DailyExpenditureTag::where('user_id', $userId)
+            ->get();
+    }
+
+    public function createDailyExpenditure(
+        int $userId,
+        string $tagName,
+    ): DailyExpenditureTag {
+        return DailyExpenditureTag::create([
+            'user_id'      => $userId,
+            'tag_name' => $tagName
+        ]);
+    }
+
+    public function deleteDailyExpenditure(int $id): void
+    {
+        DailyExpenditureTag::whereKey($id)->delete();
+    }
+}
