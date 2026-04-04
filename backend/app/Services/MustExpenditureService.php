3499af71b05174b786a5fdc9b8be017f08691a5f
molhot <sakata19991214@gmail.com>
2026-04-05 00:41:38 +0900

    service層の追加

---
+++ backend/app/Services/MustExpenditureService.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\MustExpenditure;
+use Illuminate\Database\Eloquent\Collection;
+use Carbon\Carbon;
+
+class MustExpenditureService
+{
+    public function getAllUserMustExpenditure(int $userId): Collection
+    {
+        return DailyExpenditure::where('user_id', $userId)
+            ->orderBy('created_at', 'desc')
+            ->get();
+    }
+
+    public function createMustExpenditure(
+        int $userId,
+        string $expenseName,
+        Carbon $expenseAt
+    ): MustExpenditure {
+        return MustExpenditure::create([
+            'user_id'      => $userId,
+            'expense_name' => $expenseName
+        ]);
+    }
+
+    public function deleteMustExpenditure(int $id): void
+    {
+        MustExpenditure::whereKey($id)->delete();
+    }
+}
