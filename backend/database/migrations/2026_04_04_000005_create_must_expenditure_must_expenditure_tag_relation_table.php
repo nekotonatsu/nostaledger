175eb097c7faa18ae63223706cb2ee7e8c885ced
molhot <sakata19991214@gmail.com>
2026-04-05 02:12:43 +0900

    migrationファイルの追加

---
+++ backend/database/migrations/2026_04_04_000005_create_must_expenditure_must_expenditure_tag_relation_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('must_expenditure_must_expenditure_tag_relations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete()->comment('必須出資に関わるタグを登録したユーザーのID');
+            $table->foreignId('must_expenditure_id')->constrained()->cascadeOnDelete()->comment('必須出資ID');
+            $table->foreignId('must_expenditure_tag_id')->constrained()->cascadeOnDelete()->comment('必須出資タグID');
+            $table->timestamps();
+
+            $table->index(
+                ['user_id', 'must_expenditure_id', 'must_expenditure_tag_id'],
+                'idx_user_must_expenditure_enpenditure_tag_relation'
+            );
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('must_expenditure_must_expenditure_tag_relations');
+    }
+};
---
+++ backend/database/migrations/2026_04_04_000007_create_daily_expenditure_daily_expenditure_tag_relation_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('daily_expenditure_daily_expenditure_tag_relations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete()->comment('必須出資に関わるタグを登録したユーザーのID');
+            $table->foreignId('daily_expenditure_id')->constrained()->cascadeOnDelete()->comment('出資ID');
+            $table->foreignId('daily_expenditure_tag_id')->constrained()->cascadeOnDelete()->comment('出資タグID');
+            $table->timestamps();
+
+            $table->index(
+                ['user_id', 'daily_expenditure_id', 'daily_expenditure_tag_id'],
+                'idx_user_daily_expenditure_daily_enpenditure_tag_relation'
+            );
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('daily_expenditure_daily_expenditure_tag_relations');
+    }
+};
