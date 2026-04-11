c57e1e0e71697f33cbfc495c1beb161a9bfb560a
molhot <sakata19991214@gmail.com>
2026-04-11 19:19:30 +0900

    不要なimportがあり削除

--- frontend/src/pages/regist/RegistAccount.tsx
+++ frontend/src/pages/regist/RegistAccount.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useAuth } from '../../contexts/AuthContext'
-import { Link } from 'react-router-dom'
 
 export default function RegistAccount() {
   const { register } = useAuth()
